d335a728673cdec57eb9559abb41d0ae3f3b03a9
Markus Janasch <mjanasch@kth.se>
2017-11-14 17:04:43 +0100

    Testing modified version of parameter sampling, setting Km values for ATPsyn very high, suspected to reduce influece of ATPsyn on control, adding modified parameter sampling and script to call this modified sampling

---
+++ parameter_sampling/MJanasch_CBB_ScriptToCallParameterSamplingFunction_MOD.m
@@ -0,0 +1,25 @@
+% This script calls the fast parameter sampling and coefficient calculating 
+% function for the CBB Model, adapted from Murabito et al., 2014 (Steuer)
+% Markus Janasch, Ph.D. Student, KTH
+% Created: 2017-10-12, last modified: -
+
+%% Add SBML-folders to path on Monty
+addpath('/hdd/common/tools/sbml/libSBML-5.15.0-matlab/')
+addpath('/hdd/common/tools/sbml/SBMLToolbox-4.1.0/')
+
+%% Add path to the Parameter Sampling function
+
+addpath('/ssd/common/proj/Kinetic_Model/maks/parameter_sampling/');
+
+MetConcData_RAW = importdata(MetConcSamplingData);
+
+if exist('ModelType')
+    if ~strncmp(ModelType,'CBB',3) && ~strncmp(ModelType,'XFPK',4) && ~strncmp(ModelType,'XFPK_REG',8)
+        fprintf('%s\n', 'ModelType not specified. Choose either CBB or XFPK or XFPK_REG');
+    else  
+        [DataOut] = MJanasch_Parameter_Sampling_Code_MOD(Iterations,InputDataStructure,MetConcData_RAW.data(NrOfMetDataSet,:),MetConcData_RAW.textdata,ModelType);
+        save(DataSetOut,'DataOut');
+    end
+else
+    fprintf('%s\n', 'ModelType not specified. Choose either CBB or XFPK or XFPK_REG');
+end
