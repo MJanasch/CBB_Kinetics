544c0046d14163cf46ae3a3f6757bed3d85b1e2d
Markus Janasch <mjanasch@kth.se>
2018-01-07 18:22:16 +0100

    Unfinished files for CCC plotting, added because I messed up git directories

--- parameter_sampling/data_analysis/extract_metabolite_header.m
+++ parameter_sampling/data_analysis/extract_metabolite_header.m
@@ -1,5 +1,6 @@
 % Extract the metbolites header from a CBB SKM model file
 load(model_file);
-header = char({N.species.name}.');
+Floating_Metabolites = N.species(1:size(SFull,1));
+header = char({Floating_Metabolites.name}.');
 outfile = fullfile(outdir, 'cbb_met_list.long.txt');
 dlmwrite(outfile, header, 'delimiter', '');
